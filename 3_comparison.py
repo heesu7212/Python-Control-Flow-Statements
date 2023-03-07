32ebc22a2162281ce5c2c91392c7dc6ca4cb13d1
heesu7212 <yoonghethsiu@gmail.com>
2023-03-07 17:56:56 +0900

    Learning Comparison Operator in Python

---
+++ 3_comparison.py
@@ -0,0 +1,13 @@
+print('1 == 1', 1 == 1)
+print('1 == 2', 2 == 1)
+print('1 < 2', 1 < 2)
+print('1 > 2', 1 > 2)
+print('1 >= 1', 1 >= 1)
+print('2 >= 1', 2 >= 1)
+print('1 != 1', 1 != 1)
+print('2 != 1', 2 != 1)
+
+#error: print(1 = 1)
+#need 2 equal signs
+
+

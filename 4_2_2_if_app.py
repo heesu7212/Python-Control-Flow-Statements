a3d34370e32d097effb12c21c5897c67e0126492
heesu7212 <yoonghethsiu@gmail.com>
2023-03-14 13:21:50 +0900

    Learning Conditional Statements (2-2) in Python

---
+++ 4_2_2_if_app.py
@@ -0,0 +1,15 @@
+input_id = input('id : ')
+id = 'egoing'
+
+# 1
+if input_id == id:
+    print('Welcome')
+    
+if input_id != id:
+    print('Who?')
+
+# 2
+if input_id == id:
+    print('Welcome')
+else:
+    print('Who?')
